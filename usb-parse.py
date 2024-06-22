0021abdeb35594971be19de00dc3ef40f7c2689d
Roman Penyaev <r.peniaev@gmail.com>
2024-06-22 23:23:08 +0200

    usb-parse: add license
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- usb-parse.py
+++ usb-parse.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-
+#
+# usb-parse - Reads CSV file with data extracted from oscilloscope
+#             by the `ds1054z` tool and parses USB 2.0 protocol.
+#
+#   Copyright (C) 2024 Roman Penyaev <r.peniaev@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from dataclasses import dataclass, field
 from optparse import OptionParser
 import csv
